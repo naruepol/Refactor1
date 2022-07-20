f58d244bb328c0203756dd4277c59d96878a77f4
naruapon <naruapon@webmail.npru.ac.th>
2022-07-20 11:52:17 +0700

    Write Structure Shape and Rectangle Class and Add Test Case
    Write Structure Shape and Rectangle Class and Add Test Case

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -6,8 +6,10 @@ class RectangleTest {
 
 	@Test
 	void testGetArea() {
-		Rectangle r1 = Ractangle(6,5);
+		Rectangle r1 = new Rectangle(6,5);
 		assertEquals(30,r1.getArea());
+		Rectangle r2 = new Rectangle(2,4);
+		assertEquals(8,r2.getArea());	
 	}
 
 }

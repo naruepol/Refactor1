75c192407765a518673119546cec5482450907b3
naruapon <naruapon@webmail.npru.ac.th>
2022-07-20 11:27:18 +0700

    Initial commit

---
+++ src/RectangleTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RectangleTest {
+
+	@Test
+	void testGetArea() {
+		Rectangle r1 = Ractangle(6,5);
+		assertEquals(30,r1.getArea());
+	}
+
+}
